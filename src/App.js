92cc9a60436e80288b25ce68be4ab10781da5399
donggune kim <kdg96g@gmail.com>
2022-02-04 01:59:23 +0900

    feat: three js react
    circularLight.jsx
    textureSphere.jsx
    meshes.jsx

---
+++ src/App.js
@@ -0,0 +1,9 @@
+// import Space from "./Space";
+import TextureSphere from "./TextureSphere";
+export default function App() {
+  return (
+    <div className="App">
+      <TextureSphere />
+    </div>
+  );
+}
---
+++ src/TextureSphere.jsx
@@ -0,0 +1,77 @@
+import * as THREE from "three";
+import React, { Suspense, useRef } from "react";
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
+import { OrbitControls, Stats } from "@react-three/drei";
+import "./styles.css";
+
+const Sphere = () => {
+  const sphereRef = useRef();
+  const repeatX = 3;
+  const repeatY = 2;
+
+  const [base, bump, normal, ao, metal, rough, glass] = useLoader(
+    // THREE.TextureLoader,
+    // [
+    //   "img/metal/metal1_basecolor.jpg",
+    //   "img/metal/metal1_height.png",
+    //   "img/metal/metal1_normal.jpg",
+    //   "img/metal/metal1_ambientOcclusion.jpg",
+    //   "img/metal/metal1_metallic.jpg",
+    //   "img/metal/metal1_roughness.jpg"
+    // ]
+    THREE.TextureLoader,
+    [
+      "img/stained/Glass_Stained_001_basecolor.jpg",
+      "img/stained/Glass_Stained_001_height.png",
+      "img/stained/Glass_Stained_001_normal.jpg",
+      "img/stained/Glass_Stained_001_ambientOcclusion.jpg",
+      "img/stained/Glass_Stained_001_metallic.jpg",
+      "img/stained/Glass_Stained_001_roughness.jpg",
+      "img/stained/Glass_Stained_001_Glass.jpg",
+    ]
+  );
+
+  [base, bump, normal, ao, metal, rough, glass].forEach((texture) => {
+    texture.wrapS = THREE.RepeatWrapping;
+    texture.wrapT = THREE.RepeatWrapping;
+    texture.repeat.set(repeatX, repeatY);
+  });
+
+  useFrame(() => {
+    sphereRef.current.rotation.x += 0.01;
+    sphereRef.current.rotation.y += 0.01;
+  });
+
+  return (
+    <mesh ref={sphereRef}>
+      <sphereGeometry args={[3, 36, 36]} />
+      <meshPhysicalMaterial
+        map={base}
+        bumpMap={bump}
+        aoMap={ao}
+        normalMap={normal}
+        metalnessMap={metal}
+        roughnessMap={rough}
+        glass={glass}
+      />
+    </mesh>
+  );
+};
+
+const TextureSphere = () => {
+  return (
+    <Canvas>
+      <pointLight position={[10, 0, -10]} intensity={3} />
+      <pointLight position={[-3, 0, 10]} intensity={3} />
+      <pointLight position={[2, -10, 0]} intensity={3} />
+      <ambientLight />
+      <Suspense fallback={null}>
+        <Sphere />
+      </Suspense>
+      <OrbitControls />
+      <Stats />
+    </Canvas>
+  );
+};
+
+export default TextureSphere;
