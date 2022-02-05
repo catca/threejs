7cdd998a3f02f991dddcf8c1bb7128ea50b8aa29
donggune kim <kdg96g@gmail.com>
2022-02-05 10:16:15 +0900

    feat: add cameraControl.jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 // import Space from "./Space";
 // import TextureSphere from "./TextureSphere";
-import ToggleApp from "./ToggleApp";
+// import ToggleApp from "./ToggleApp";
+import CameraControl from "./CameraControl";
 export default function App() {
   return (
     <div className="App">
-      <ToggleApp />
+      <CameraControl />
     </div>
   );
 }
